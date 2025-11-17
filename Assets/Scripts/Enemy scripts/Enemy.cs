1eae08a7c8b88d075a48cd606dfa5df6af82220c
Emil Madsen <Nesstark0@gmail.com>
2025-11-17 12:20:46 +0100

    Revert "Not working shit"
    This reverts commit 1298657845e46d2cc6a6c7bcf57ba71f35c90df3.

--- Assets/Scripts/Enemy scripts/Enemy.cs
+++ Assets/Scripts/Enemy scripts/Enemy.cs
@@ -5,21 +5,14 @@ public class EnemyCombat : Unit
     [Header("Which collider should trigger damage?")]
     public Collider damageTrigger;
 
-    private Animator animator;
-    public RagdollJointFollower[] followers;
-    private ConfigurableJoint[] joints;
-
-    protected override void Awake()
+    protected override void Awake()  // change to private if Unit has no Awake()
     {
-        base.Awake();
-
-        animator = GetComponent<Animator>();
-        followers = GetComponentsInChildren<RagdollJointFollower>();
-        joints = GetComponentsInChildren<ConfigurableJoint>();
+        base.Awake();                // remove this line if Unit has no Awake()
 
         if (damageTrigger != null)
             damageTrigger.isTrigger = true;
 
+        // Add DamageForwarder to the trigger collider
         if (damageTrigger != null)
         {
             DamageForwarder forwarder = damageTrigger.gameObject.AddComponent<DamageForwarder>();
@@ -39,23 +32,9 @@ public class EnemyCombat : Unit
     {
         base.Die();
 
-        // 1. Disable animation driver
-        if (animator != null)
-            animator.enabled = false;
-
-        // 2. Disable active-ragdoll joint followers
-        foreach (var f in followers)
-            f.enabled = false;
-
-        // 3. OPTIONAL: Make joints fully floppy by removing motor force
-        JointDrive zero = new JointDrive { positionSpring = 0, positionDamper = 0, maximumForce = 0 };
-        foreach (var j in joints)
-        {
-            j.angularXDrive = zero;
-            j.angularYZDrive = zero;
-            j.slerpDrive = zero;
-        }
-
-        // Now the enemy is a FULL floppy ragdoll both in VR and physics
+        // Remove all Configurable Joints
+        ConfigurableJoint[] joints = GetComponentsInChildren<ConfigurableJoint>();
+        foreach (var joint in joints)
+            Destroy(joint);
     }
 }
